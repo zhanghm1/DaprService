43d6fb32b91cc1f1a768020c3177ce307ba887d3
xxxx <xxxx@xxx.com>
2021-08-14 11:44:10 +0800

    添加项目文件。

---
+++ DaprService/Controllers/OrderController.cs
@@ -0,0 +1,63 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DaprTest.OrderApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly DaprClient _daprClient;
+        public OrderController(ILogger<OrderController> logger, DaprClient daprClient)
+        {
+            _logger = logger;
+            _daprClient = daprClient;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<string>>  Get()
+        {
+            await _daprClient.PublishEventAsync("order", "newOrder", new JianKuCunDto
+            {
+                ProductId = 1,
+                Model = "32G",
+                Number = 1
+
+            });
+
+            return await _daprClient.InvokeMethodAsync<IEnumerable<string>>(HttpMethod.Get,"productapi", "Product");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(AddOrderDto addOrder)
+        {
+            await _daprClient.PublishEventAsync("order", "newOrder", new JianKuCunDto
+            {
+                ProductId = addOrder.ProductId,
+                Model = addOrder.Model,
+                Number = addOrder.Number
+
+            });
+            return Ok();
+        }
+    }
+    public class AddOrderDto
+    {
+        public int MemberId { get; set; }
+        public int ProductId { get; set; }
+        public string Model { get; set; }
+        public int Number { get; set; }
+    }
+    public class JianKuCunDto
+    {
+        public int ProductId { get; set; }
+        public string Model { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ DaprTest.ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,103 @@
+﻿using Dapr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaprTest.ProductApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> Products;
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+            Products = new List<Product>() {
+                new Product(){
+                    Id=1,
+                    Name="iPhone X",
+                    Models=new List<ProductModel>(){
+                        new ProductModel(){
+                            Id=1,
+                            ProductId=1,
+                            Model="64G",
+                            Number=100
+                        },
+                        new ProductModel(){
+                            Id=1,
+                            ProductId=1,
+                            Model="128G",
+                            Number=100
+                        },
+                    }
+                },
+                new Product(){
+                    Id=2,
+                    Name="iPhone XR",
+                    Models=new List<ProductModel>(){
+                        new ProductModel(){
+                            Id=1,
+                            ProductId=1,
+                            Model="64G",
+                            Number=100
+                        },
+                        new ProductModel(){
+                            Id=1,
+                            ProductId=1,
+                            Model="128G",
+                            Number=100
+                        },
+                    }
+                },
+            };
+        }
+
+        [HttpGet]
+        public async Task<List<Product>>  Get()
+        {
+            return Products;
+        }
+        /// <summary>
+        /// 减库存
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [Topic("order", "newOrder")]
+        [HttpPut]
+        public async Task<IActionResult> Put(JianKuCunDto request)
+        {
+            var productModel = Products.Where(a => a.Id == request.ProductId)?.FirstOrDefault().Models.Where(a => a.Model == request.Model)?.FirstOrDefault();
+            productModel.Number -= request.Number;
+            return Ok();
+        }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ProductModel> Models { get; set; }
+    }
+    /// <summary>
+    /// 型号
+    /// </summary>
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Model { get; set; }
+        public int Number { get; set; }
+    }
+    public class JianKuCunDto
+    { 
+        public int ProductId { get; set; }
+        public string Model { get; set; }
+        public int Number { get; set; }
+    }
+}
